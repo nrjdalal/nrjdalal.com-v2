bbc38496925c42f3d9d558f317c22ef01b4a38e3
Neeraj Dalal <admin@nrjdalal.com>
2025-01-30 03:48:06 +0530

    feat: enabled toc

--- src/components/mdx/code.tsx
+++ src/components/mdx/code.tsx
@@ -1,17 +1,23 @@
 "use client"
 
 import { useCopyToClipboard } from "@/hooks/use-copy"
+import { useState } from "react"
 
 export default function Component(props: {
   children: React.ReactNode
   __raw__?: string
 }) {
-  const [copiedText, copy] = useCopyToClipboard()
+  const [, copy] = useCopyToClipboard()
+  const [buttonText, setButtonText] = useState("Copy")
 
   const handleCopy = (text: string) => () => {
     copy(text)
       .then(() => {
+        setButtonText("Copied")
         console.log("Copied!", { text })
+        setTimeout(() => {
+          setButtonText("Copy")
+        }, 2000)
       })
       .catch((error: Error) => {
         console.error("Failed to copy!", error)
@@ -21,14 +27,16 @@ export default function Component(props: {
   return !props.__raw__ ? (
     <code>{props.children}</code>
   ) : (
-    <div className="relative overflow-scroll rounded-md bg-black p-5 text-white">
+    <div className="relative">
       <button
         className="absolute top-4 right-4 cursor-pointer rounded-sm bg-white px-2 text-black"
         onClick={handleCopy(props.__raw__)}
       >
-        {copiedText === props.__raw__ ? "Copied" : "Copy"}
+        {buttonText}
       </button>
-      {props.children}
+      <div className="overflow-scroll rounded-md bg-black p-5 text-white">
+        <code>{props.children}</code>
+      </div>
     </div>
   )
 }
