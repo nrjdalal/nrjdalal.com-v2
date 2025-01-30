a462aefcb3812ef8ac32724d59413e9927baa324
Neeraj Dalal <admin@nrjdalal.com>
2025-01-30 21:04:10 +0530

    feat: added header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,12 @@
 
 import ThemeToggle from "@/components/theme-toggle"
 import { cn } from "@/lib/utils"
-import { RiCodeAiFill, RiGithubFill, RiTwitterXFill } from "@remixicon/react"
+import {
+  RiBrushAiFill,
+  RiCodeAiFill,
+  RiGithubFill,
+  RiTwitterXFill,
+} from "@remixicon/react"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 
@@ -11,6 +16,13 @@ const navItems = [
   { name: "blogs", href: "/blogs" },
   { name: "projects", href: "/projects" },
   { name: "contact", href: "/contact" },
+]
+
+const uiNavItems = [
+  { name: "home", href: "/ui" },
+  { name: "docs", href: "/ui/docs" },
+  { name: "components", href: "/ui/components" },
+  { name: "blocks", href: "/ui/blocks" },
 ]
 
 const socialLinks = [
@@ -23,14 +35,49 @@ export default function Component() {
 
   return (
     <div className="bg-background sticky top-0 z-50 flex h-14 w-full justify-between border-b">
-      <Link
-        href="/"
-        className="hover:bg-border/50 flex h-full min-w-64 cursor-pointer items-center justify-center gap-x-2 border-r px-5"
+      <div className="flex">
+        <Link
+          href="/"
+          className={cn(
+            "hover:bg-border/50 flex h-full min-w-64 cursor-pointer items-center justify-center gap-x-2 border-r px-5",
+            pathname.startsWith("/ui") && "hidden",
+          )}
+        >
+          <RiCodeAiFill className="size-6" />
+          <p className="font-mono">NRJDALAL.COM</p>
+        </Link>
+        <Link
+          href="/ui"
+          className={cn(
+            "hover:bg-border/50 flex h-full cursor-pointer items-center justify-center gap-x-2 border-r px-5",
+            pathname.startsWith("/ui") && "min-w-64",
+          )}
+        >
+          <RiBrushAiFill className="size-6" />
+          <p
+            className={cn("font-mono", !pathname.startsWith("/ui") && "hidden")}
+          >
+            NRJDALAL.COM/UI
+          </p>
+        </Link>
+        <Link
+          href="/"
+          className={cn(
+            "hidden",
+            pathname.startsWith("/ui") &&
+              "hover:bg-border/50 flex h-full cursor-pointer items-center justify-center gap-x-2 border-r px-5",
+          )}
+        >
+          <RiCodeAiFill className="size-6" />
+        </Link>
+      </div>
+
+      <div
+        className={cn(
+          "text-muted-foreground flex cursor-pointer items-center divide-x font-medium",
+          pathname.startsWith("/ui") && "hidden",
+        )}
       >
-        <RiCodeAiFill className="size-6" />
-        <p className="font-mono">NRJDALAL.COM</p>
-      </Link>
-      <div className="text-muted-foreground flex cursor-pointer items-center divide-x font-medium">
         {navItems.map((item) => (
           <Link
             key={item.name}
@@ -54,6 +101,41 @@ export default function Component() {
             <link.icon className="size-6" />
           </Link>
         ))}
+
+        <ThemeToggle />
+      </div>
+
+      <div
+        className={cn(
+          "hidden",
+          pathname.startsWith("/ui") &&
+            "text-muted-foreground flex cursor-pointer items-center divide-x font-medium",
+        )}
+      >
+        {uiNavItems.map((item) => (
+          <Link
+            key={item.name}
+            href={item.href}
+            className={cn(
+              "hover:text-foreground hover:border-b-foreground flex h-full items-center px-7.5 hover:border-b-2 hover:pt-[2px]",
+              pathname === item.href &&
+                "text-foreground border-b-foreground border-b-2 pt-[2px]",
+            )}
+          >
+            {item.name}
+          </Link>
+        ))}
+        {socialLinks.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            target="_blank"
+            className="hover:text-foreground hover:border-foreground flex aspect-square h-full items-center justify-center hover:border-t-2 hover:border-r-2 hover:pb-[2px] hover:pl-[1px]"
+          >
+            <link.icon className="size-6" />
+          </Link>
+        ))}
+
         <ThemeToggle />
       </div>
     </div>
